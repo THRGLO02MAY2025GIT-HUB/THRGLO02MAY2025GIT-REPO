470693dce7dbcf6f6ca59fdfde1da8280ab2faca
DEV <DEV@DEV.COM>
2025-05-13 10:34:26 +0530

    commit

--- Demos/ld-java-17/ld-java-17/src/annotations/ModelToJsonConverter.java
+++ Demos/ld-java-17/ld-java-17/src/annotations/ModelToJsonConverter.java
@@ -4,7 +4,11 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ModelToJsonConverter {
     // Check if the object is not nul, else
@@ -36,11 +40,34 @@ public class ModelToJsonConverter {
     private String getJsonString(Object object) throws IllegalAccessException {
        // Lab : Create a Map containing the JSON field and the Value and return a JSON String
 //        Expected Output :  {"firstName":"John", "lastName":"rambo", "successfulTransactionFormatted":"50"}
-       return null;
+        Class<?> clazz = object.getClass();
+        // Populate the map with fields
+        Map<String, String> jsonElementMap = new HashMap<>();
+        for(Field field : clazz.getDeclaredFields()){
+           field.setAccessible(true);
+           if(field.isAnnotationPresent(JsonElement.class)){
+//               jsonElementMap.put(field.getName(), (String)field.get(object));
+               jsonElementMap.put(getKey(field), (String)field.get(object));
+           }
+        }
+        System.out.println("DEBUG : " + jsonElementMap);
+
+        Set<Map.Entry<String,String>> jsonElementEntrySet = jsonElementMap.entrySet();
+        String jsonString = jsonElementEntrySet
+                .stream()
+                .map(entry -> "\"" + entry.getKey() + "\":\"" + entry.getValue() + "\"")
+                .collect(Collectors.joining(","));
+        System.out.println("DEBUG : " + jsonString);
+       return "Expected output : " + "{" + jsonString + "}";
     }
     private String getKey(Field field) {
-       // Write the logic actually get the key="successfulTransactionFormatted"
-      return null;
+      // Write the logic actually get the key="successfulTransactionFormatted"
+      String value = field.getAnnotation(JsonElement.class).key();
+      if(value.isEmpty()){
+          return field.getName();
+      } else {
+          return value;
+      }
     }
 
     public String convertToJson(Object object) throws JsonSerializationException {
